3dfc391a2813283806edce9987f8d0beb1fd7121
benji77fr <b.girardpro@gmail.com>
2023-04-22 16:46:40 +0200

    add zod schema for login form

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,26 +1,32 @@
 import { useForm, SubmitHandler } from 'react-hook-form';
-
-type TLoginInput = {
-  username: string;
-  password: string;
-};
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
 
 function Login() {
+  const validationLoginSchema = z.object({
+    username: z.string().nonempty(),
+    password: z.string().nonempty(),
+  })
+  
+  type TValidationLoginSchema = z.infer<typeof validationLoginSchema>;
+
   const {
     register,
     handleSubmit,
-    formState: { errors, isSubmitted },
-  } = useForm<TLoginInput>();
+    formState: { errors, isSubmitted, isSubmitting },
+  } = useForm<TValidationLoginSchema>({
+    resolver: zodResolver(validationLoginSchema),
+  });
 
-  const onSubmit: SubmitHandler<TLoginInput> = (data) => {
+  const onSubmit: SubmitHandler<TValidationLoginSchema> = (data) => {
     console.log(data);
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register('username')} />
+      <input {...register('username')} disabled={isSubmitting} />
       {errors.username && <span>This field is required</span>}
-      <input {...register('password')} />
+      <input {...register('password')} disabled={isSubmitting}/>
       <input type="submit" disabled={!isSubmitted} />
     </form>
   );
