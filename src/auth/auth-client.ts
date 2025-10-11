9513a3cca1c8da494e739ee26e3c1b3145006054
Damien Arnaud <damien.arnaudpro@gmail.com>
2025-10-11 16:34:28 +0200

    Login : Added auth with better-auth (client, service, hook)

---
+++ src/auth/auth-client.ts
@@ -0,0 +1,8 @@
+import {createAuthClient} from "better-auth/react"
+
+type BetterAuthClient = ReturnType<typeof createAuthClient>
+
+export const authClient:BetterAuthClient = createAuthClient({
+    baseURL: `${import.meta.env.VITE_BACKEND_URL}/auth`,
+})
+
---
+++ src/auth/useAuth.ts
@@ -0,0 +1,20 @@
+
+import {SignInParams, SignUpParams} from "../types/authTypes.ts";
+import {AuthService} from "../api/auth.service.ts";
+
+export const useAuth = () => {
+
+    const signIn = async (params: SignInParams) => {
+        return await AuthService.signIn(params)
+
+    }
+    const signUp = async (params: SignUpParams) => {
+        return await AuthService.signUp(params)
+    }
+
+    const signOut = async (callbackURL?: string) => {
+        return await AuthService.signOut({callbackURL})
+    }
+
+    return {signIn, signUp, signOut}
+}
